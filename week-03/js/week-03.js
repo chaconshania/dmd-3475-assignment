95380526fed12fd416cf172ddaead51ca269cc89
Shania Chacon <chacon1shania@gmail.com>
2025-02-10 18:01:32 -0500

    input changes the text in webpage

--- week-03/js/week-03.js
+++ week-03/js/week-03.js
@@ -2,4 +2,7 @@ let usersName = prompt("Welcome, what is your first name?");
 usersName = usersName.toUpperCase();
 
 console.log(`Hello ${usersName}! Welcome to my website!`);
-window.prompt(`Hello ${usersName}! Welcome to my website`);
+
+let message = document.getElementById("message");
+
+message.innerHTML = `Welcome ${usersName} to my simple webpage!`;
