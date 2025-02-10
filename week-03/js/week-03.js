5a7d6ab15e8171da17f4980ab9580bd02eb50b7e
Shania Chacon <chacon1shania@gmail.com>
2025-02-10 17:55:16 -0500

    wrote prompt that asks for user's name and displays name all in upper case in the console.

---
+++ week-03/js/week-03.js
@@ -0,0 +1,5 @@
+let usersName = prompt("Welcome, what is your first name?");
+usersName = usersName.toUpperCase();
+
+console.log(`Hello ${usersName}! Welcome to my website!`);
+window.prompt(`Hello ${usersName}! Welcome to my website`);
